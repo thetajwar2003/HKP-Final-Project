357ccd70ab51ddbf045c35ad0dab1498cb1ee3db
kzhang01 <karinazhang1@gmail.com>
2021-01-11 14:50:44 -0500

    initial commit
    Added base code from CleverCart and fixed some errors so now it compiles

---
+++ CleverCart/CleverCart/CartView.swift
@@ -0,0 +1,32 @@
+//
+//  CartView.swift
+//  CleverCart
+//
+//  Created by Tajwar Rahman on 1/8/21.
+//  Copyright © 2021 Tajwar Rahman. All rights reserved.
+//
+
+import SwiftUI
+
+struct CartView: View {
+    @Binding var screen: Int
+    @ObservedObject var cart: Cart
+    @ObservedObject var handler = UserHandler()
+    
+    var body: some View {
+        //Placeholder, delete later
+        Text("CartView")
+        NavigationView {
+            List {
+                ForEach(cart.cart, id: \.self) { item in
+                    Text(item.name)
+                    Text("\(item.quantity)")
+                }.onDelete(perform: deleteItem)
+            }
+        }
+    }
+    func deleteItem(at offsets: IndexSet) {
+        cart.cart.remove(atOffsets: offsets)
+    }
+}
+
---
+++ CleverCart/CleverCart/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  CleverCart
+//
+//  Created by Tajwar Rahman on 1/8/21.
+//  Copyright © 2021 Tajwar Rahman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @ObservedObject var items = Items()
+    @State private var screen = 0
+    @ObservedObject var cart = Cart()
+    var body: some View {
+        Group {
+            if screen == 0 {
+                LoginView(screen: $screen)
+            }
+            if screen == 1{
+                SignUpView(screen: $screen)
+            }
+            if screen == 2{
+                ItemView(screen: $screen)
+            }
+            if screen == 3{
+                CartView(screen: $screen, cart: cart)
+            }
+        }
+        .environmentObject(items)
+        .environmentObject(cart)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
