dd56915d9f9e35fee9668ef31cfcde3937cecda5
kzhang01 <karinazhang1@gmail.com>
2021-01-12 14:41:57 -0500

    addItem + admin/user options
    Created AddItemView and made different NavigationBarItems show up depending on if the user is an admin or not. isAdmin value is hardcoded for now because we don't have any users.

---
+++ CleverCart/CleverCart/ImagePicker.swift
@@ -0,0 +1,43 @@
+//
+//  ImagePicker.swift
+//  CleverCart
+//
+//  Created by Karina Zhang on 1/12/21.
+//  Copyright © 2021 Tajwar Rahman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var image: UIImage?
+        
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        var parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+    func makeCoordinator() -> ImagePicker.Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+    }
+}
--- CleverCart/CleverCart/ItemView.swift
+++ CleverCart/CleverCart/ItemView.swift
@@ -11,41 +11,76 @@ import SwiftUI
 struct ItemView: View {
     @Binding var screen: Int
     @State private var items = Items()
+    @State private var showingImagePicker = false
+    @State private var addingNewItem = false
+    @State private var goingToCart = false
+    
+    //placeholder to test the different screens, shouldn't be hardcoded like this
+    @State private var isAdmin = true
+    
+    @State private var inputImage: UIImage?
     @ObservedObject var handler = UserHandler()
+    
+    
+    var addNewItem: some View {
+        Button(action: {
+            self.addingNewItem = true
+        }) {
+            Image(systemName: "plus")
+                .padding(10)
+        }
+        .sheet(isPresented: $addingNewItem) {
+            AddItemView(quantity: 1)
+        }
+        
+    }
+    
+    var goToCart: some View {
+        Button(action: {
+            self.goingToCart = true
+        }) {
+            Image("cart")
+                .padding(10)
+        }
+        .sheet(isPresented: $goingToCart) {
+            //temporary
+            CartView(screen: self.$screen, cart: Cart())
+
+//            self.screen = 3
+        }
+    }
     
     var body: some View {
         NavigationView {
             //Placeholder, delete later
-            VStack {
-                Text("ItemView")
-                
-                Text("Go to cart")
-                    .onTapGesture {
-                        self.screen = 3
-                    }
-            }
+            Text("ItemView")
             
             
-            List {
-                ForEach(items.items, id: \.self) { item in
-                    HStack {
-                        VStack {
-                            Text("\(item.name)")
-                                .font(.title)
-                            Text("Quantity: \(item.quantity)")
-                        }
-                        
-                        Spacer()
-                        
-                        Button("Add") {
-                            Image(systemName: "plus")
-                            .onTapGesture {
-                                self.handler.addItem(token: "", name: item.name, quantity: "\(item.quantity)", link: "https://hkp-ios-demo-api.herokuapp.com/items/create")
-                            }
-                        }
-                    }
-                }
-            }
+//            List {
+//                ForEach(items.items, id: \.self) { item in
+//                    HStack {
+//                        VStack {
+//                            Text("\(item.name)")
+//                                .font(.title)
+//                            Text("Quantity: \(item.quantity)")
+//                        }
+//
+//                        Spacer()
+//
+//                        Button("Add") {
+//                            Image(systemName: "plus")
+//                            .onTapGesture {
+//                                self.handler.addItem(token: "", name: item.name, quantity: "\(item.quantity)", link: "https://hkp-ios-demo-api.herokuapp.com/items/create")
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+            .navigationBarTitle("Shop")
+            .navigationBarItems(leading: isAdmin ? AnyView(EditButton()) : AnyView(EmptyView()), trailing: isAdmin ? AnyView(addNewItem) : AnyView(goToCart))
+            
+            
+            
         }
     }
 }
